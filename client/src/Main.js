3f136b4d4d799dbcf5e78ff95d5db629c76c89a4
gali gurevich <galiagurevich@gmail.com>
2021-09-04 21:52:10 +0300

    1.1

--- client/src/Main.js
+++ client/src/Main.js
@@ -1,21 +1,21 @@
 import React from 'react';
 import './Main.css';
 
-class Main extends React.Component {
+function Main(props) {
 
-  render() {
+  const [result, setResult] = React.useState(props.dataObject);
+  console.log(result)
+  React.useEffect(() => {
+    setResult(result);
+  }, [result]);
 
-
-
-   return (
-      <div className="container">
-
-        <div id="palceholder1" className="placeholder">hello {this.props.responseCurrency[0]}</div>
-        <div id="palceholder2" className="placeholder"></div>
-        <div id="palceholder3" className="placeholder"></div>
-      </div>
-    );
-  }
+  return <div className="container">
+  {result[0]}
+  <div id="palceholder1" className="placeholder">hello</div>
+  <div id="palceholder2" className="placeholder"></div>
+  <div id="palceholder3" className="placeholder"></div>
+  </div>;
 }
+
 
 export default Main;
